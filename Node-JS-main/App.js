60e566beec5a2131e030e2e7b0dbac9dfca145db
nikhil-kumar6 <97690669+nikhil-kumar6@users.noreply.github.com>
2022-06-24 11:30:02 +0530

    Project update

---
+++ Node-JS-main/App.js
@@ -0,0 +1,28 @@
+    
+    //  WHAT IS NODE JS :-
+       //    Node.js is a cross-platform environment and library for
+      //     JavaScript applications which is used to create networking 
+      //      and server-side applications.
+
+
+
+// console.log("Hello world")
+
+
+        //every 3 second 
+
+
+// setTimeout(function()  {
+//     console.log('3 seconds have passed');
+// }, 3000);
+
+    
+ 
+        //Every 2sec it vll add 
+
+// var time=0;
+// setInterval(function()  {
+//     time+=2;
+//     console.log(time + 'seconds have passed');
+    
+// }, 2000);
---
+++ Node-JS-main/App1.js
@@ -0,0 +1,5 @@
+var stuff =require('./stuff');
+
+console.log(stuff.counter(['king','queen','danu']));
+console.log(stuff.adder(5,5));
+console.log(stuff.adder(stuff.pi,5));
